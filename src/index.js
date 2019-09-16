d46482b1b600b70937667c36506bc53ce626c1fe
José Tuzinkievicz <josetuzin@gmail.com>
2019-09-16 10:04:24 -0300

    clase 12

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// la ruta empieza sin ./ porque es un archivo de npm
+import 'bootstrap/dist/css/bootstrap.css'
+import './global.css'
+
+import Badge from './components/Badge'
+
+
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+// no olvidar los < /> para transformar el 
+// componente en elemento
+ReactDOM.render(
+    <Badge 
+        firstName="Jose" 
+        lastName="Tuzin" 
+        avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+        jobTitle="Frontend Engineer" 
+        twitter="sparragus" 
+    />, 
+    container);
