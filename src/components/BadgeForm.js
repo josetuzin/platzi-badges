9d075e353ff516bc078bff08940141560ead36f2
José Tuzinkievicz <josetuzin@gmail.com>
2019-09-16 15:48:15 -0300

    clase 16 levantamiento de estado

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,25 +2,27 @@ import React from 'react'
 
 class BadgeForm extends React.Component {
 
-    state = {
-        firstName:"",
-        lastName:"",
-        email:"",
-        jobTitle:"",
-        twitter:""
-      };
+    state = { 
+        form: {   // inicializamos los valores para que no de warning
+            firstName: '',  
+            lastName: '',  
+            email: '',  
+            jobTitle: '',  
+            twitter: '',  
+        }
+    };
 
-    // por convencion, el metodo que maneja el 
-    // change del textbox se llama handleChange
-    handleChange = (e) => {
-        // console.log({ 
-        //     name: e.target.name,    
-        //     value: e.target.value 
-        // })
-        this.setState({  // método de la clase Component
-            [e.target.name]: e.target.value, // para que actualice el valor de cada textbox
-        })  
-    }
+    //// por convencion, el metodo que maneja el 
+    //// change del textbox se llama handleChange
+    //// handleChange = (e) => {
+    //     // console.log({ 
+    //     //     name: e.target.name,    
+    //     //     value: e.target.value 
+    //     // })
+    //     this.setState({  // método de la clase Component
+    //         [e.target.name]: e.target.value, // para que actualice el valor de cada textbox
+    //     })  
+    // }
 
     handleClick = e => {
         console.log("Me tocaste el botón")
@@ -40,46 +42,52 @@ class BadgeForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>Firstname</label>
-                        <input onChange={this.handleChange} 
+                        <input 
+                            onChange={this.props.onChange} 
                             className="form-control" 
-                            type="text" name="firstName"
-                            value={this.state.firstName}    // input controlado
+                            type="text" 
+                            name="firstName"
+                            value={this.props.formValues.firstName}    // input controlado
                         />
                     </div>
                     <div className="form-group">
                         <label>Lastname</label>
-                        <input onChange={this.handleChange} 
+                        <input 
+                            onChange={this.props.onChange} 
                             className="form-control" 
                             type="text" 
                             name="lastName"
-                            value={this.state.lastName}
+                            value={this.props.formValues.lastName}
                         />
                     </div>
                     <div className="form-group">
                         <label>Email</label>
-                        <input onChange={this.handleChange} 
+                        <input 
+                            onChange={this.props.onChange} 
                             className="form-control" 
                             type="email" 
                             name="email"
-                            value={this.state.email}
+                            value={this.props.formValues.email}
                         />
                     </div>
                     <div className="form-group">
                         <label>Job Title</label>
-                        <input onChange={this.handleChange} 
+                        <input 
+                            onChange={this.props.onChange} 
                             className="form-control" 
                             type="text" 
                             name="jobTitle"
-                            value={this.state.jobTitle}
+                            value={this.props.formValues.jobTitle}
                         />
                     </div>
                     <div className="form-group">
                         <label>Twitter</label>
-                        <input onChange={this.handleChange} 
+                        <input 
+                            onChange={this.props.onChange} 
                             className="form-control" 
                             type="text"
                             name="twitter"
-                            value={this.state.twitter}
+                            value={this.props.formValues.twitter}
                         />
                     </div>
 
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -6,7 +6,20 @@ import Navbar from '../components/Navbar'
 import Badge from '../components/Badge'
 import BadgeForm from '../components/BadgeForm'
 
+
+// página
 class BadgeNew extends React.Component {
+
+    state = { form: {} }
+
+    handleChange = e => {
+        this.setState({
+            form: {  // esto se hace para agregar datos de a 1, y no sobreescribir todo
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        }) 
+    }
 
     render() {
         return (
@@ -20,15 +33,16 @@ class BadgeNew extends React.Component {
                     <div className="row">
                         <div className="col-6">
                             <Badge 
-                                firstName="Richard"
-                                lastName="Kaufman"
-                                twitter="sparragus"
-                                jobTitle="Frontend"
+                                firstName={this.state.form.firstName}
+                                lastName={this.state.form.lastName}
+                                twitter={this.state.form.twitter}
+                                jobTitle={this.state.form.jobTitle}
+                                email={this.state.form.email}
                                 avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
                             />
                         </div>
                         <div className="col-6">
-                            <BadgeForm />
+                            <BadgeForm onChange={this.handleChange} formValues={this.state.form}/>
                         </div>
                     </div>
                 </div>
