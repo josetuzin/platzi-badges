a7d2159923f59e412162fa674d04df52dbd4dda3
José Tuzinkievicz <josetuzin@gmail.com>
2019-09-16 11:00:24 -0300

    clase 13

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
 
-import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 
 const container = document.getElementById('app');
@@ -13,11 +13,5 @@ const container = document.getElementById('app');
 // no olvidar los < /> para transformar el 
 // componente en elemento
 ReactDOM.render(
-    <Badge 
-        firstName="Jose" 
-        lastName="Tuzin" 
-        avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-        jobTitle="Frontend Engineer" 
-        twitter="sparragus" 
-    />, 
+    <BadgeNew/>, 
     container);
